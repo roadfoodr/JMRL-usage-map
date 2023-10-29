fe7e24e9cd03049b5c193f2459eafed45779a8b3
M Powers <mpowers@alum.mit.edu>
2023-10-29 01:48:47 -0400

    Add global filter control

--- streamlit_app.py
+++ streamlit_app.py
@@ -8,6 +8,9 @@ Created on Wed May 24 02:35:20 2023
 import pandas as pd
 import numpy as np
 import streamlit as st
+from st_files_connection import FilesConnection
+
+from streamlit_utilities import check_password as check_password
 
 import random
 
@@ -17,8 +20,11 @@ import matplotlib.pyplot as plt
 # import altair as alt
 # import pydeck as pdk
 
-DATA_DIR = './data/'
-DATA_FILE = 'patrons_geocoded_grouped_091923.xlsx'
+# DATA_DIR = './data/'
+DATA_DIR = 'jmrl-visualization/'
+# DATA_FILE = 'patrons_geocoded_grouped_091923.xlsx'
+# DATA_FILE = 'patrons_geocoded_grouped_091923.csv'
+DATA_FILE = 'patrons_geocoded_091923.csv'
 
 map_style = 'mapbox://styles/mapbox/streets-v12'
 
@@ -26,18 +32,9 @@ map_style = 'mapbox://styles/mapbox/streets-v12'
 @st.cache_data
 def load_data(data_file):
     path = f'{DATA_DIR}{data_file}'
-    
-    skiprows_p = 1.0  # e.g. 100% of the lines
-    # https://stackoverflow.com/questions/22258491/read-a-small-random-sample-from-a-big-csv-file-into-a-python-data-frame
-    # keep the header, then take only p% of lines
 
-    df = pd.read_excel(
-        path,
-        # nrows=100,
-        nrows=None,
-        skiprows=lambda i: i>0 and random.random() > skiprows_p,
-        header=0,
-    )
+    conn = st.experimental_connection('s3', type=FilesConnection)
+    df = conn.read(path, input_format="csv", ttl=600)
         
     return df
 
@@ -47,44 +44,106 @@ st.set_page_config(
     page_icon=":book:",
     page_title="JMRL usage")  # Intentionally obscure
 
-df_grouped = load_data(DATA_FILE)
-# TODO temporarily removing a specific outlier pending data exploration
-# df_grouped = df_grouped[df_grouped['geoloc'] != 'dqb0q5']
+if not check_password():
+    st.stop()
+    # pass
+
+df = load_data(DATA_FILE)
 
 st.write("## JMRL usage")
 
+
+# %% filter and rename
+df['lat'] = df['lat_orig']
+df['lon'] = df['long_orig']
+
+usecols = ['TOT CHKOUT', 'TOT RENWAL', 
+       'creation_date', 'home_branch', 'jurisdiction',
+       'card_type', 'lat', 'lon', 'geoloc',
+       'lat_geohash', 'long_geohash', 'frequent_location',
+       'frequent_location_tie']
+
+df = df[usecols]
+
+df.dropna(subset=['lat', 'lon'], inplace=True)
+# df['color'] = [(0.25, 0.4, 1.0, 0.1)] * len(df)
+
+df['color'] = "#ffaa0030"
+
+# %% preview df
+
+st.write(f"##### Sample of data: {len(df)} rows")
+st.dataframe(df.head(5))
+
+
+# %% global filter
+st.write("##### Global subset ")
+
+col11, col12 = st.columns(2)
+with col11:
+    global_filter = 'All'
+    global_filter_options = {'All': 'All patrons',
+                             'jurisdiction': 'Jurisdiction',
+                             'home_branch': 'Home Branch',
+                             'frequent_location': 'Frequent Branch',
+                             }
+
+    global_filter_field = st.selectbox(
+        'Global filter by:',
+        global_filter_options.keys(),
+        format_func=lambda x: global_filter_options[x]
+        )
+    
+    if global_filter_field != 'All':
+        global_filter_vals = df[global_filter_field].unique()
+        global_filter_vals = global_filter_vals[~pd.isnull(global_filter_vals)]
+        global_filter_vals = global_filter_vals[global_filter_vals != 'none']
+        global_filter_vals = global_filter_vals[global_filter_vals != 'Historical Society']
+        global_filter_choices = np.sort(global_filter_vals)
+        with col12:
+            global_filter_selection = st.selectbox(
+                f'{global_filter_options[global_filter_field]} to filter by', 
+                ['All'] + list(global_filter_choices))
+        
+    df_filtered = (df if (global_filter_field == 'All'
+                          or global_filter_selection == 'All')
+                   else df[df[global_filter_field] == global_filter_selection].copy()
+                   )
+st.write(f'Rows in current view: {len(df_filtered)}')
+st.dataframe(df_filtered.head(5))
+
 # %% set up color column
+if False:
+    # https://stackoverflow.com/questions/47398081/how-do-i-map-df-column-values-to-hex-color-in-one-go
+    # https://matplotlib.org/stable/users/explain/colors/colormaps.html
+    
+    # color_source_col = 'Patron_count'
+    color_source_col = 'Circ'
+    # cmap = plt.cm.viridis
+    # cmap = plt.cm.Reds
+    # cmap = plt.cm.plasma
+    # cmap = plt.cm.RdBu_r
+    # cmap = plt.cm.PuRd
+    cmap = plt.cm.BuGn
+    # cmap = plt.cm.coolwarm
+    
+    # TODO: select the high_clip as a percentile of values, provide slider control
+    norm = mcolors.Normalize(vmin=np.nanmin(df_grouped[color_source_col].values),
+                               vmax=np.nanmax(df_grouped[color_source_col].values), 
+                              # vmax=11900, 
+                             clip=True)
+    mapper = plt.cm.ScalarMappable(norm=norm, cmap=cmap)
+    
+    # df_grouped['color'] = df_grouped[color_source_col].apply(lambda x: mcolors.to_hex(mapper.to_rgba(x)))
+    df_grouped['color'] = df_grouped[color_source_col].apply(lambda x: mapper.to_rgba(x, alpha=.4))
 
-# https://stackoverflow.com/questions/47398081/how-do-i-map-df-column-values-to-hex-color-in-one-go
-# https://matplotlib.org/stable/users/explain/colors/colormaps.html
 
-# color_source_col = 'Patron_count'
-color_source_col = 'Circ'
-# cmap = plt.cm.viridis
-# cmap = plt.cm.Reds
-# cmap = plt.cm.plasma
-# cmap = plt.cm.RdBu_r
-# cmap = plt.cm.PuRd
-cmap = plt.cm.BuGn
-# cmap = plt.cm.coolwarm
-
-# TODO: select the high_clip as a percentile of values, provide slider control
-norm = mcolors.Normalize(vmin=np.nanmin(df_grouped[color_source_col].values),
-                           vmax=np.nanmax(df_grouped[color_source_col].values), 
-                          # vmax=11900, 
-                         clip=True)
-mapper = plt.cm.ScalarMappable(norm=norm, cmap=cmap)
-
-# df_grouped['color'] = df_grouped[color_source_col].apply(lambda x: mcolors.to_hex(mapper.to_rgba(x)))
-df_grouped['color'] = df_grouped[color_source_col].apply(lambda x: mapper.to_rgba(x, alpha=.4))
-
-
-st.write(f"##### Sample of data: {len(df_grouped)} rows")
-st.dataframe(df_grouped.head(5))
 
 # %% display map
-st.write("##### Map: aggregated patrons")
+st.write("##### Map: selected patrons")
+st.write(df_filtered['color'].value_counts())
+# st.write(df_filtered.dtypes)
+df_filtered['color'] = "#ffaa0030"
 
-st.map(data=df_grouped, zoom=9, color='color', size=300)
-
-
+# st.map(data=df_filtered, zoom=9, color='color', size=50)
+st.map(data=df_filtered, zoom=9, color='#6babd030', size=10)
